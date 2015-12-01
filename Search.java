e78d77550ed089da11377f03a709ee3b048a6df9
Barry <log525lg100@gmail.com>
2015-12-01 12:17:44 +1100

    commit for previous algorithm works

---
+++ Search.java
@@ -0,0 +1,300 @@
+package alogorithm;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.function.Function;
+
+class Search {
+
+	private static class Data{
+		private int[] books;
+		private int[] box;
+		private int n;
+		Data(int n) {
+			super();
+			this.books = new int[n+1];
+			this.box = new int[n+1];
+			this.n = n;
+		}
+	}
+
+	private static class Holder{
+		private int[][] next={{0,1},{1,0},{0,-1},{-1,0}};
+		private int rescueP;
+		private int rescueQ;
+		private int n;
+		private int m;
+		private int min=9999;
+		private int[][] books;
+		private int[][] map;
+		
+		Holder(int p,int q,int n,int m) {
+			super();
+			this.rescueP = p;
+			this.rescueQ = q;
+			this.n=n;
+			this.m=m;
+			this.books=new int[n+1][m+1];
+			this.map=new int[n+1][m+1];
+			setObstacle();
+		}
+		
+		private void setObstacle(){
+			this.map[1][3]=1;
+			this.map[3][3]=1;
+			this.map[4][2]=1;
+			this.map[5][4]=1;
+		}
+		
+		void printPath(){
+			for (int i=1;i<=n;i++){
+				for (int j=1;j<=m;j++){
+					if (books[i][j]==1){
+						System.out.print(i+","+j+" ");
+					}
+				}
+			}
+		}
+	}
+
+	
+	private static class Node{
+		private int x;
+		private int y;
+		private int step;
+		private int parent;
+	}
+
+	/**
+	 * depth first search
+	 * try every possibilities, such as loop the number of cases
+	 * concerning how to deal with the problem at this step in the loop,
+	 * and recursive call itself since the next step should be the same as previous
+	 * end condition(solved the problem) at the front of dfs
+	 * @param step - the number for a box
+	 */
+	private static void dfs(int step,Function<Data,String> result,Data holder){
+		if (step==holder.n+1){// end condition:at the end of search by reaching the step bigger than maximum
+			result.apply(holder);
+			return;
+		}
+		for (int i=1;i<=holder.n;i++){
+			if (holder.books[i]==0){
+				holder.box[step]=i;
+				holder.books[i]=1;
+				dfs(step+1,result,holder);
+				holder.books[i]=0;//set flag to indicate the number is ready to use for next iteration
+			}
+		}
+	}
+
+	public static void dfsForAllpossibilities(){
+		Data pokerHolder=new Data(5);//list all combination of numbers
+		Data equationHolder=new Data(9);//list all combination of numbers to make [][][]+[][][]=[][][]
+		Function<Data,String> pokerResult=(t)->{
+			for (int i=1;i<=t.n;i++){
+				System.out.print(t.box[i]+" ");
+			}
+			System.out.println();
+			return "";
+		};
+		Function<Data,String> equationResult=(t)->{
+			if ((t.box[1] * 100 + t.box[2] * 10 + t.box[3] + t.box[4] * 100
+					+ t.box[5] * 10 + t.box[6]) == (t.box[7] * 100 + t.box[8]
+					* 10 + t.box[9])) {
+				System.out.println(Arrays.toString(t.box));
+			}
+			return "";
+		};
+		dfs(1,pokerResult,pokerHolder);// start at the beginning
+	}
+
+	/**
+	 * 第四章- 解救小哈
+	 * @param x - current location in the row
+	 * @param y - current location in the column
+	 * @param step - steps has been taken
+	 * @param holder - holder for data used in the program
+	 */
+	private static void rescue(int x,int y,int step,Holder holder){
+		if (x==holder.rescueP&&y==holder.rescueQ){
+			if (step<=holder.min){
+				holder.min=step;
+				holder.printPath();
+				System.out.println();
+			}
+			return;
+		}
+		for (int i=0;i<=3;i++){
+			int tx=x+holder.next[i][0];
+			int ty=y+holder.next[i][1];
+			if (tx <= holder.n && tx >= 1 && ty >= 1 && ty <= holder.m
+					&& holder.books[tx][ty] == 0 && holder.map[tx][ty] == 0) {
+				holder.books[tx][ty] = 1;
+				rescue(tx, ty, step + 1, holder);
+				holder.books[tx][ty] = 0;
+			}
+		}
+	}
+
+	/**
+	 * 第四章- 解救小哈
+	 */
+	public static void rescueWithDFS(){
+		Holder holder=new Holder(4,3,5,4);
+		holder.books[1][1]=1;
+		rescue(1,1,0,holder);
+		System.out.println(holder.min);
+	}
+	
+
+	/**
+	 * 第四章- 解救小哈
+	 * solve by breath first search
+	 * try every possible moves at each step, 
+	 * save them in the moves storage if they are ok to move(there are some contraints)
+	 * move the step to next one after finishing trying
+	 * among each possible moves, check if it solves the problems,
+	 * if so jump out the iteration
+	 * @param n
+	 * @param m
+	 * @param rescueP
+	 * @param rescueQ
+	 */
+	public static void rescueWithBFS(int n,int m,int rescueP,int rescueQ){
+		Node[] que=new Node[2501];// step storage implemented by queue
+		for (int i=1;i<2501;i++){
+			que[i]=new Node();
+		}
+		int head,tail;
+		int[][] map=new int[51][51];
+		int[][] books=new int[51][51];// stores passed location
+		int[][] next={{0,1},{1,0},{0,-1},{-1,0}};
+		map[1][3]=1;//set obstacle
+		map[3][3]=1;
+		map[4][2]=1;
+		map[5][4]=1;//set obstacle
+		head=tail=1;
+		que[tail].x=1;// start at 1
+		que[tail].y=1;// start at 1
+		que[tail].step=0;
+		que[tail].parent=0;
+		tail++;
+		books[1][1]=1;
+		boolean success = false;
+		while(head<tail){
+			for (int i=0;i<=3;i++){
+				int tx=que[head].x+next[i][0];
+				int ty=que[head].y+next[i][1];
+				if (tx<=n&&tx>=1&&ty<=m&&ty>=1&&books[tx][ty]==0&&map[tx][ty]==0){
+					books[tx][ty]=1;
+					que[tail].x=tx;
+					que[tail].y=ty;
+					que[tail].step=que[head].step+1;
+					que[tail].parent=head;
+					tail++;
+					if (tx==rescueP&&ty==rescueQ){
+						int current=tail-1;
+						System.out.println(que[current].step);
+						while(current>=1){
+							System.out.print(que[current].x+", "+que[current].y+" -> ");
+							current=que[current].parent;
+						}
+						success=true;
+						break;
+					}
+				}
+			}
+			if (success){break;}
+			head++;
+		}
+	}
+	
+	private static int calculate(char[][] map,int x,int y,int[] direction){
+		int sum=0;int i=x,j=y;
+		while(map[i][j]!='#'){
+			if (map[i][j]=='G'){
+				sum++;
+			}
+			i=i+direction[0];
+			j=j+direction[1];
+		}
+		return sum;
+	}
+
+	private static char[][] readMap(int n,int m){
+		String dir=System.getProperty("user.dir");
+		char[][] map=new char[n+1][m+1];BufferedReader reader=null;
+		try {
+			reader=new BufferedReader(new FileReader(dir+"\\src\\alogorithm\\map.txt"));
+			String line;int i=1;
+			while((line=reader.readLine())!=null){
+				char[] t=line.toCharArray();
+				for (int j=1;j<=m;j++){
+					map[i][j]=t[j-1];
+				}
+				i++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return map;
+	}
+
+	private static void boomManWithBFS(int startX,int startY,int n,int m){
+		Node[] que=new Node[401];
+		for (int i=1;i<401;i++){
+			que[i]=new Node();
+		}
+		int head,tail;
+		char[][] map=readMap(n,m);
+		int[][] books=new int[n+1][m+1];
+		int[][] next={{0,1},{1,0},{0,-1},{-1,0}};
+		head=tail=1;
+		que[tail].x=startX;
+		que[tail].y=startY;
+		tail++;
+		books[startX][startY]=1;
+		int sum=0,locX=0,locY=0;
+		for (int i=0;i<4;i++){
+			sum+=calculate(map,que[head].x,que[head].y,next[i]);
+		}
+		while(head<tail){
+			for (int i=0;i<4;i++){
+				int tx=que[head].x+next[i][0];
+				int ty=que[head].y+next[i][1];
+				if (tx<n&&tx>1&&ty<m&&ty>1&&books[tx][ty]==0&&map[tx][ty]=='.'){
+					books[tx][ty]=1;
+					que[tail].x=tx;
+					que[tail].y=ty;
+					tail++;
+					int t=0;
+					for (int j=0;j<4;j++){
+						t+=calculate(map,tx,ty,next[j]);
+					}
+					if (t>sum){sum=t;locX=tx;locY=ty;}
+				}
+			}
+			head++;
+		}
+		System.out.println("max "+sum+" locX "+locX+" locY "+locY);
+	}
+
+
+	public static void main(String[] args) {
+//		dfsForAllpossibilities();
+		rescueWithDFS();
+		rescueWithBFS(5,4,4,3);
+		System.out.println();
+		boomManWithBFS(4,4,13,13);
+	}
+
+}
---
+++ StackQueueLinkedList.java
@@ -0,0 +1,269 @@
+package alogorithm;
+
+
+public class StackQueueLinkedList {
+
+
+	private static void queueDecode(int[] arr){
+		int[] result=new int[2*arr.length-2];
+		for (int i=0;i<arr.length;i++){
+			result[i]=arr[i];
+		}
+		int head=0,tail=arr.length-1;
+		while(head+1!=tail){
+			tail++;
+			head++;
+			result[tail]=result[head];
+			head++;
+		}
+		for (int i=0;i<result.length-2;i+=2){
+			System.out.print(result[i]+" ");
+		}
+		System.out.print(result[result.length-2]+" ");
+		System.out.print(result[result.length-1]);
+	}
+
+	/**
+	 * tail of queue is last index, not set to next index of last one
+	 * so make sure increment the tail before append an element to the queue
+	 * @param arr
+	 */
+	private static void queueDecodeTwo(int[] arr){
+		int[] result=new int[2*arr.length-2];
+		for (int i=0;i<arr.length;i++){
+			result[i]=arr[i];
+		}
+		Queue q=new Queue(0,arr.length-1,result);
+		while(q.getHead()+1!=q.getTail()){
+			q.setTail(q.getTail()+1);
+			q.setHead(q.getHead()+1);
+			int[] data=q.getData();
+			data[q.getTail()]=data[q.getHead()];
+			q.setHead(q.getHead()+1);
+		}
+		int[] data=q.getData();
+		for (int i=0;i<data.length-2;i+=2){
+			System.out.print(data[i]+" ");
+		}
+		System.out.print(data[data.length-2]+" ");
+		System.out.println(data[data.length-1]);
+	}
+
+	/**
+	 * stack has top index pointed to last element in the list
+	 * @param str
+	 */
+	private static void plalindromeCheckWithStack(String str){
+		int len=str.length();
+		if (len%2==0){System.out.println("not plalindrome");return;}
+		int mid=len/2;
+		char[] stack=new char[mid];
+		int top=0;
+		for (int i=0;i<mid;i++){
+			stack[top]=str.charAt(i);
+			top++;
+		}
+		boolean flag=true;
+		for(int i=1;i<=mid;i++){
+			if (str.charAt(mid+i)!=stack[top-1]){
+				flag=false;
+				break;
+			}
+			top--;
+		}
+		if (flag){
+			System.out.println("plalindrome");
+		}else{
+			System.out.println("not plalindrome");
+		}
+	}
+
+	/**
+	 * µÚ¶þÕÂÖ½ÅÆÓÎÏ·-Ð¡Ã¨µöÓã
+	 * @param p1
+	 * @param p2
+	 */
+	private static void game(int[] p1,int[] p2){
+		Stack table=new Stack(new int[9],-1);
+		int[] p1d=new int[100];
+		int[] p2d=new int[100];
+		int[] books=new int[9];
+		for (int i=0;i<p1.length;i++){
+			p1d[i]=p1[i];
+			p2d[i]=p2[i];
+		}
+		Queue p1q=new Queue(0,p1.length-1,p1d);
+		Queue p2q=new Queue(0,p2.length-1,p2d);
+		int current=1;
+		while(p1q.getHead()<=p1q.getTail()&&p2q.getHead()<=p2q.getTail()){
+			if (current==1){
+				play(p1q,table,books);
+				current=2;
+			}else{
+				play(p2q,table,books);
+				current=1;
+			}
+		}
+		if (p2q.getHead()>p2q.getTail()){
+			printMessage(p1q,1,table);
+		} else if(p1q.getHead()>p1q.getTail()){
+			printMessage(p2q,2,table);
+		}
+	}
+
+	private static void printMessage(Queue q,int p,Stack table){
+		if (p==1){
+			System.out.println("player 1 win");
+		}else{
+			System.out.println("player 2 win");
+		}
+		System.out.println("player has cards");
+		for (int i=q.getHead();i<=q.getTail();i++){
+			System.out.print(q.getData()[i]+" ");
+		}
+		System.out.println();
+		if (table.getTop()>=0){
+			System.out.println("cards on the table");
+			for (int i=0;i<=table.getTop();i++){
+				System.out.print(table.getData()[i]+" ");
+			}
+			System.out.println();
+		}else{
+			System.out.println(" no card on the table");
+		}
+	}
+
+	private static void play(Queue q,Stack table,int[] books){
+		int card=q.getData()[q.getHead()];
+		if (books[card]==0){
+			q.setHead(q.getHead()+1);
+			table.setTop(table.getTop()+1);
+			table.getData()[table.getTop()]=card;
+			books[card]=1;
+		}else{
+			q.setHead(q.getHead()+1);
+			q.setTail(q.getTail()+1);
+			q.getData()[q.getTail()]=card;
+			while(table.getData()[table.getTop()]!=card){
+				int t=table.getData()[table.getTop()];
+				q.setTail(q.getTail()+1);
+				q.getData()[q.getTail()]=t;
+				books[t]=0;
+				table.setTop(table.getTop()-1);
+			}
+		}
+	}
+
+	/**
+	 * at list elements in the ascending ordered array
+	 * @param arr
+	 * @return Node - the head pointer of the linked list
+	 */
+	private static Node linkedList(int[] arr){
+		Node[] linkedList=new Node[arr.length];
+		linkedList[0]=new Node(arr[0]);
+		for (int i=1;i<arr.length;i++){
+			Node t=new Node(arr[i]);
+			linkedList[i-1].setNext(t);
+			linkedList[i]=t;
+		}
+		Node head=new Node(0);
+		head.setNext(linkedList[0]);
+		return head;
+	}
+
+	/**
+	 * insert an element into a linked list pointed by a head pointer
+	 * @param head
+	 * @param number
+	 */
+	private static void insertNode(Node head,int number){
+		Node current,prev=head;
+		while((current=prev.getNext())!=null){
+			if (current.getData()>=number){
+				Node temp=new Node(number);
+				prev.setNext(temp);
+				temp.setNext(current);
+				break;
+			}
+			prev=current;
+		}
+	}
+
+	/**
+	 * create a linked list from an array
+	 * @param arr
+	 * @return
+	 */
+	private static LinkedList linkedListTwo(int[] arr){
+		int[] data=new int[100];
+		int[] right=new int[100];
+		for (int i=0;i<arr.length-1;i++){
+			data[i]=arr[i];
+			right[i]=i+1;
+		}
+		data[arr.length-1]=arr[arr.length-1];
+		return new LinkedList(data,right,arr.length);
+	}
+
+	/**
+	 * insert at that index would just insert it after the element of that index
+	 * @param list
+	 * @param number
+	 * @param index
+	 */
+	private static void insertAt(LinkedList list,int number,int index){
+		int[] data=list.getData();
+		int[] right=list.getRight();
+		int tail=list.getTail();
+		data[tail]=number;
+		int t=right[index];
+		right[index]=tail;
+		right[tail]=t;
+		list.setTail(tail+1);
+	}
+	
+	/**
+	 * insert at the end of the list
+	 * @param list
+	 * @param number
+	 */
+	private static void insert(LinkedList list,int number){
+		int tail=list.getTail();
+		insertAt(list,number,tail-1);
+	}
+
+	private static void printList(LinkedList list){
+		int[] data=list.getData();
+		int[] right=list.getRight();
+		System.out.print("["+data[0]+", ");
+		int nexti=right[0];int tail=list.getTail()-1;
+		while(tail!=1){
+			System.out.print(data[nexti]+", ");
+			nexti=right[nexti];
+			tail--;
+		}
+		System.out.println(data[nexti]+"]");
+	}
+
+
+	public static void main(String[] args) {
+		int[] arr={6,3,1,7,5,8,9,2,4};
+		queueDecode(arr);
+		System.out.println();
+		queueDecodeTwo(arr);
+		plalindromeCheckWithStack("xy2azay5x");
+		int[] p1={2,4,1,2,5,6};
+		int[] p2={3,1,3,5,6,4};
+		game(p1,p2);
+		int[] list={2,4,6,8,10};
+		Node head=linkedList(list);
+		insertNode(head,3);
+		int[] listTwo={2,3,5,8,9,10,18,26,32};
+		LinkedList linklist=linkedListTwo(listTwo);
+		insertAt(linklist,6,0);
+		System.out.println(linklist);
+		printList(linklist);
+	}
+
+}
